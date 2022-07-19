d436f35f0da5a3b8a42b7efacd5c4ef6f031bdac
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-19 20:03:49 +0200

    add starter code

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
 import sqlite3
 
+# database name
+db_name = "student.db"
+
 # SQL queries
 CREATE_TABLE = """ CREATE TABLE IF NOT EXISTS student 
-(id INTEGER PRIMARY KEY, first_name TEXT, middle_name TEXT, last_name TEXT,
-gender TEXT, age INTEGER, email TEXT, mobile TEXT,  Nationality TEXT) """
+(id TEXT, first_name TEXT, middle_name TEXT, last_name TEXT,
+gender TEXT, age INTEGER, email TEXT, mobile TEXT,  nationality TEXT, degree TEXT) """
 
-INSERT_STUDENT = """ INSERT INTO student (first_name, middle_name, last_name, gender, age, email, mobile, Nationality) VALUES (?, ?, ?, ?, ?, ?, ?, ?) """
+INSERT_STUDENT = """ INSERT INTO student (id, first_name, middle_name, last_name, gender, age, email, mobile, nationality, degree) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) """
+
 SELECT_STUDENT = """ SELECT * FROM student WHERE id = ? """
 UPDATE_STUDENT = """ UPDATE student SET first_name = ?, middle_name = ?, last_name = ?, gender = ?, age = ? email = ?, mobile = ?, Nationality = WHERE id = ? """
 DELETE_STUDENT = """ DELETE FROM student WHERE id = ? """
 SELECT_ALL = """ SELECT * FROM student """ 
+
 
 # Create a new database if the database doesn't already exist
 def create_database(db_name):
@@ -19,14 +24,28 @@ def create_database(db_name):
     conn.commit()
     conn.close()
 
-
-# get student by name
-
-# get all list of students
-
 # add student to database
+def add_student(db_name, student):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute(INSERT_STUDENT, (student.id, student.firstName, student.middleName, student.lastName, student.gender, student.age, student.email, student.phone, student.nationality, student.degree))
+    conn.commit()
+    conn.close()
 
-# update student in database
 
-# remove student from database
+# get student by id
+def getStudent(db_name, id):
+    pass
+
+# get all students
+def getAllStudent(db_name):
+    pass
+
+# update student in database given by id
+def updateStudent(db_name, id):
+    pass
+
+# delete student from database by id
+def deleteStudent(db_name, id):
+    pass
 
